0aa0078f51ca2bfe6dc8362dad44a3c642d0f4cd
Adriano A. Ramos <47153274+AdrianoaRamos@users.noreply.github.com>
2022-09-24 23:26:47 -0300

    Update dados.js

--- src/dados.js
+++ src/dados.js
@@ -2,7 +2,7 @@ const cards = [
     {
         title: "404-not-found",
         description: "Págiana de erro desenvolvida a partir de um desafio proposto pelo site FrontEnd Mentor.",
-        imagem: "https://raw.githubusercontent.com/AdrianoaRamos/portfolio/master/src/img/404.jpg",
+        imagem: "https://raw.githubusercontent.com/AdrianoaRamos/portifolio/master/src/img/404.jpg",
         repositorio: "https://github.com/AdrianoaRamos/404-not-found",
         primeiroIcone:"fa-brands fa-html5",
         segundoIcone:"fa-brands fa-css3-alt",
@@ -11,7 +11,7 @@ const cards = [
     {
         title: "Site de turismo espacial",
         description: "Construir um site de turismo espacial de várias páginas e fazê-lo parecer o mais próximo possível do design.",
-        imagem:"https://raw.githubusercontent.com/AdrianoaRamos/portfolio/master/src/img/viagem-espacial.JPG",
+        imagem:"https://raw.githubusercontent.com/AdrianoaRamos/portifolio/master/src/img/viagem-espacial.JPG",
         repositorio: "https://github.com/AdrianoaRamos/space-tourism-website-main",
         primeiroIcone:"fa-brands fa-html5",
         segundoIcone:"fa-brands fa-css3-alt",
@@ -20,7 +20,7 @@ const cards = [
     {
         title: "Parallax",
         description: "Estudo de aplicação do efeito Parallax em uma página. Com esse efeito é possível expressar a profundidade e a perspectiva em um site controlando a quantidade de deslocamento de um elemento específico.",
-        imagem:"https://raw.githubusercontent.com/AdrianoaRamos/portfolio/master/src/img/paralax.jpg",
+        imagem:"https://raw.githubusercontent.com/AdrianoaRamos/portifolio/master/src/img/paralax.jpg",
         repositorio: "https://github.com/AdrianoaRamos/parallaxcblol",
         primeiroIcone:"fa-brands fa-html5",
         segundoIcone:"fa-brands fa-css3-alt",
@@ -29,7 +29,7 @@ const cards = [
     {
         title: "Em Construção",
         description: "Pegue o café e espere um pouco, estou desenvolvendo o projeto...",
-        imagem:"https://raw.githubusercontent.com/AdrianoaRamos/portfolio/master/src/img/construcao.jpg",
+        imagem:"https://raw.githubusercontent.com/AdrianoaRamos/portifolio/master/src/img/construcao.jpg",
         repositorio: "https://github.com/AdrianoaRamos"
     }
 ]
